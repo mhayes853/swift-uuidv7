deca62adea3eb4f95b2d580d6ede40390ebb0cd5
mhayes853 <matthewha853@gmail.com>
2025-08-05 11:49:32 -0700

    Revert "Add IdentifierStringConvertible conformance to tagged UUIDV7s"
    This reverts commit 040315154f4c489b8685087c31e6ed56a96f214e.

--- Sources/UUIDV7/UUIDV7+SharingGRDB.swift
+++ Sources/UUIDV7/UUIDV7+SharingGRDB.swift
@@ -1,10 +1,6 @@
 #if SwiftUUIDV7SharingGRDB
   import SharingGRDBCore
 
-  #if SwiftUUIDV7Tagged
-    import Tagged
-  #endif
-
   extension UUIDV7: IdentifierStringConvertible {
     public var rawIdentifier: String { self.uuidString }
 
@@ -12,14 +8,4 @@
       self.init(uuidString: rawIdentifier)
     }
   }
-
-  #if SwiftUUIDV7Tagged
-    extension Tagged: @retroactive IdentifierStringConvertible where RawValue == UUIDV7 {
-      public var rawIdentifier: String { self.uuidString }
-
-      public init?(rawIdentifier: String) {
-        self.init(uuidString: rawIdentifier)
-      }
-    }
-  #endif
 #endif
