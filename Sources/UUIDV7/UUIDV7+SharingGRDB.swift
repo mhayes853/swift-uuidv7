040315154f4c489b8685087c31e6ed56a96f214e
mhayes853 <matthewha853@gmail.com>
2025-08-05 11:33:13 -0700

    Add IdentifierStringConvertible conformance to tagged UUIDV7s

--- Sources/UUIDV7/UUIDV7+SharingGRDB.swift
+++ Sources/UUIDV7/UUIDV7+SharingGRDB.swift
@@ -1,6 +1,10 @@
 #if SwiftUUIDV7SharingGRDB
   import SharingGRDBCore
 
+  #if SwiftUUIDV7Tagged
+    import Tagged
+  #endif
+
   extension UUIDV7: IdentifierStringConvertible {
     public var rawIdentifier: String { self.uuidString }
 
@@ -8,4 +12,14 @@
       self.init(uuidString: rawIdentifier)
     }
   }
+
+  #if SwiftUUIDV7Tagged
+    extension Tagged: @retroactive IdentifierStringConvertible where RawValue == UUIDV7 {
+      public var rawIdentifier: String { self.uuidString }
+
+      public init?(rawIdentifier: String) {
+        self.init(uuidString: rawIdentifier)
+      }
+    }
+  #endif
 #endif
