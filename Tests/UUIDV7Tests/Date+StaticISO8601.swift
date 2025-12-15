5f8f1f859ebb4051cfef085242066ffe2f6ecaf9
mhayes853 <matthewha853@gmail.com>
2025-12-14 23:43:16 -0800

    Wrap tests with conditional Foundation import

--- Tests/UUIDV7Tests/Date+StaticISO8601.swift
+++ Tests/UUIDV7Tests/Date+StaticISO8601.swift
@@ -1,9 +1,11 @@
-import Foundation
+#if canImport(Foundation)
+  import Foundation
 
-extension Date {
-  init(staticISO8601: StaticString) {
-    self = formatter.date(from: "\(staticISO8601)")!
+  extension Date {
+    init(staticISO8601: StaticString) {
+      self = formatter.date(from: "\(staticISO8601)")!
+    }
   }
-}
 
-private nonisolated(unsafe) let formatter = ISO8601DateFormatter()
+  private nonisolated(unsafe) let formatter = ISO8601DateFormatter()
+#endif
